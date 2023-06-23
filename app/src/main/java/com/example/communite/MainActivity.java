94e65dd0bacc0a1784c0bb1500614b80b7f20de1
Kristine <chankristineagcay@gmmail.com>
2023-06-23 15:34:53 +0800

    added home screen and button

--- app/src/main/java/com/example/communite/MainActivity.java
+++ app/src/main/java/com/example/communite/MainActivity.java
@@ -3,10 +3,11 @@ package com.example.communite;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private RecyclerView postList;
+    private Toolbar mToolbar;
 
 
     @Override
@@ -23,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Home");
+
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
         navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -38,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     private void UserMenuSelector(MenuItem item)
     {
         int itemId = item.getItemId();
@@ -47,8 +55,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_friends) {
             Toast.makeText(this, "Friend List", Toast.LENGTH_SHORT).show();
-        } else if (itemId == R.id.nav_find_friends) {
-            Toast.makeText(this, "Find Friends", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_messages) {
             Toast.makeText(this, "Messages", Toast.LENGTH_SHORT).show();
         } else if (itemId == R.id.nav_report) {
