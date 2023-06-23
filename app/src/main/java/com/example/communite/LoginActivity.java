a9e35942bfb9cf830429b0da5293f1ec53d9f2cb
Kristine <chankristineagcay@gmmail.com>
2023-06-24 00:53:15 +0800

    Working log in screen using the registered account

--- app/src/main/java/com/example/communite/LoginActivity.java
+++ app/src/main/java/com/example/communite/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.communite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +24,9 @@ public class LoginActivity extends AppCompatActivity {
     private EditText UserEmail, UserPassword;
     private TextView ForgotPassword;
     private TextView NeedNewAccountLink;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,24 +35,91 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        mAuth = FirebaseAuth.getInstance();
+
         NeedNewAccountLink = (TextView) findViewById(R.id.register_account_link);
         ForgotPassword = (TextView) findViewById(R.id.forgot_password);
         UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
         LoginButton = (Button) findViewById(R.id.login_button);
+        loadingBar = new ProgressDialog(this);
+
 
         NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 SendUserToRegisterActivity();
             }
+        });
 
-            private void SendUserToRegisterActivity() {
-                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(registerIntent);
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                AllowingUserToLogin();
+
 
             }
-        })
+        });
 
     ;}
-}
+
+    private void AllowingUserToLogin() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT);
+
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please enter your password.", Toast.LENGTH_SHORT);
+        }
+        else
+        {
+            loadingBar.setTitle("Login");
+            loadingBar.setMessage("Please wait while  we are allowing you to login to your account");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.signInWithEmailAndPassword(email, password)
+
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                SendUserToMainActivity();
+
+
+                               Toast.makeText(LoginActivity.this, "You are logged In successfully",Toast.LENGTH_SHORT );
+                               loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error occurred:" + message, Toast.LENGTH_SHORT);
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(mainIntent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+
+    private void SendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+    }}
