628ba041054c6ebed0ab06ae2282658d3cbfb235
Kristine <chankristineagcay@gmmail.com>
2023-07-01 18:35:00 +0800

    create new  post on main page and allow user to enter their gallery when they want to add an image

--- app/src/main/java/com/example/communite/PostActivity.java
+++ app/src/main/java/com/example/communite/PostActivity.java
@@ -3,6 +3,10 @@ package com.example.communite;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,10 @@ import androidx.appcompat.widget.Toolbar;
 public class PostActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+    private static final int Gallery_Pick = 1 ;
 
 
     @Override
@@ -18,11 +26,30 @@ public class PostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
+        SelectPostImage = (ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription =(EditText) findViewById(R.id.post_description);
+
+
         mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Update Post");
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
     }
 
     @Override
