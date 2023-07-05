625fa72c670d9adb001e3cd33d9b03da794e0156
Kristine <chankristineagcay@gmmail.com>
2023-07-05 14:20:24 +0800

    done showing edit and delete post only for user's post

--- app/src/main/java/com/example/communite/ClickPostActivity.java
+++ app/src/main/java/com/example/communite/ClickPostActivity.java
@@ -1,6 +1,7 @@
 package com.example.communite;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,12 +24,18 @@ public class ClickPostActivity extends AppCompatActivity {
     private TextView PostDescription;
     private Button DeletePostButton, EditPostButton;
     private DatabaseReference ClickPostRef;
-    private String PostKey;
+    private FirebaseAuth mAuth;
+    private String PostKey, currentUserID, databaseUserID, description, image;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_post);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+
 
         PostKey = getIntent().getStringExtra("PostKey");
         ClickPostRef = FirebaseDatabase.getInstance("https://communite-f7efa-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("Posts").child(PostKey);
@@ -37,14 +45,23 @@ public class ClickPostActivity extends AppCompatActivity {
         DeletePostButton = findViewById(R.id.delete_post_button);
         EditPostButton = findViewById(R.id.click_edit_post_button);
 
+        DeletePostButton.setVisibility(View.INVISIBLE);
+        EditPostButton.setVisibility(View.INVISIBLE);
+
+
         ClickPostRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    String description = dataSnapshot.child("description").getValue(String.class);
-                    String image = dataSnapshot.child("postimage").getValue(String.class);
+                    description = dataSnapshot.child("description").getValue(String.class);
+                    image = dataSnapshot.child("postimage").getValue(String.class);
+                    databaseUserID = dataSnapshot.child("uid").getValue().toString();
 
                     PostDescription.setText(description);
+                    if (currentUserID.equals(databaseUserID)){
+                        DeletePostButton.setVisibility(View.VISIBLE);
+                        EditPostButton.setVisibility(View.VISIBLE);
+                    }
                     Picasso.get().load(image).into(PostImage, new Callback() {
                         @Override
                         public void onSuccess() {
