d2b70e6f42eb1ae3c669787f5db119894b20f92c
Sean Harvey <seanharveyofiangga@gmail.com>
2023-07-17 23:02:32 +0800

    Created activity in progress and it's java

---
+++ app/src/main/java/com/example/communite/InProgressActivity.java
@@ -0,0 +1,14 @@
+package com.example.communite;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_progress);
+    }
+}
