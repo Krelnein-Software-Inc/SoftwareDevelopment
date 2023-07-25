58e196a07d73ab4badc7af7417c75042493e9cd1
Kristine <chankristineagcay@gmmail.com>
2023-07-25 17:51:04 +0800

    done checkbox

--- app/src/main/java/com/example/communite/ReportPageActivity.java
+++ app/src/main/java/com/example/communite/ReportPageActivity.java
@@ -16,6 +16,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,15 +32,21 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ReportPageActivity extends AppCompatActivity {
 
-    private DatabaseReference reportsRef;
+    private DatabaseReference reportsRef, DoneRef;
     private RecyclerView postsRecyclerView;
     private ReportPostAdapter reportPostAdapter;
+    private FirebaseAuth mAuth;
     private List<Reports> reportsList;
+    String currentUserID;
+    Boolean DoneChecker = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_page);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
 
         reportsRef = FirebaseDatabase.getInstance("https://communite-f7efa-default-rtdb.asia-southeast1.firebasedatabase.app")
                 .getReference()
@@ -50,6 +57,7 @@ public class ReportPageActivity extends AppCompatActivity {
         reportsList = new ArrayList<>();
         reportPostAdapter = new ReportPostAdapter(reportsList);
         postsRecyclerView.setAdapter(reportPostAdapter);
+        DoneRef = FirebaseDatabase.getInstance("https://communite-f7efa-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("Progress");
 
         reportsRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -57,10 +65,7 @@ public class ReportPageActivity extends AppCompatActivity {
                 reportsList.clear();
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     Reports reports = snapshot.getValue(Reports.class);
-
-                    // Set the PostKey for each Reports object
                     reports.setPostKey(snapshot.getKey());
-
                     reportsList.add(reports);
                 }
                 reportPostAdapter.notifyDataSetChanged();
@@ -72,31 +77,20 @@ public class ReportPageActivity extends AppCompatActivity {
             }
         });
 
-        // Find the "Report Page Back Button" ImageView by its ID
         ImageView reportPageBackButton = findViewById(R.id.report_page_back_button);
-
-        // Set an OnClickListener to the "Report Page Back Button" ImageView
         reportPageBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Go back to the main activity when the back button is clicked
                 onBackPressed();
             }
         });
 
-
-
-        // Find the "Add Report" button by its ID
         ImageView addReportButton = findViewById(R.id.add_report_button);
-
-        // Set a click listener to the "Add Report" button
         addReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Redirect to the AddReportActivity when the "Add Report" button is clicked
                 Intent addReportIntent = new Intent(ReportPageActivity.this, AddReportActivity.class);
                 startActivity(addReportIntent);
-
             }
         });
     }
@@ -107,7 +101,6 @@ public class ReportPageActivity extends AppCompatActivity {
 
         public ReportPostAdapter(List<Reports> reportsList) {
             this.reportsList = reportsList;
-
         }
 
         @NonNull
@@ -127,20 +120,42 @@ public class ReportPageActivity extends AppCompatActivity {
             holder.setPostImage(reports.getPostimage());
             holder.setPostDate(reports.getDate());
             holder.setPostTime(reports.getTime());
-
-            final String postKey = reports.getPostKey();
+            holder.setDoneButtonStatus(reports.getPostKey());
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // Redirect to the ClickReportActivity and pass the PostKey as an extra
                     Intent clickReportIntent = new Intent(ReportPageActivity.this, ClickReportActivity.class);
-                    clickReportIntent.putExtra("PostKey", postKey);
+                    clickReportIntent.putExtra("PostKey", reports.getPostKey());
                     startActivity(clickReportIntent);
                 }
             });
-        }
 
+            holder.DoneReportButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    DatabaseReference likesRef = DoneRef.child(reports.getPostKey()).child(currentUserID);
+
+                    likesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                // The user has already liked the report, so we remove the like
+                                likesRef.removeValue();
+                            } else {
+                                // The user has not liked the report, so we add the like
+                                likesRef.setValue(true);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Handle onCancelled event
+                        }
+                    });
+                }
+            });
+        }
 
         @Override
         public int getItemCount() {
@@ -149,6 +164,7 @@ public class ReportPageActivity extends AppCompatActivity {
 
         public class PostViewHolder extends RecyclerView.ViewHolder {
 
+            public ImageView DoneReportButton;
             private CircleImageView profileImage;
             private TextView profileName, postDescription, postDate, postTime;
             private ImageView postImage;
@@ -157,7 +173,6 @@ public class ReportPageActivity extends AppCompatActivity {
             public PostViewHolder(@NonNull View itemView) {
                 super(itemView);
 
-                // Initialize ViewHolder components
                 profileImage = itemView.findViewById(R.id.report_profile_image);
                 profileName = itemView.findViewById(R.id.report_user_name);
                 postDescription = itemView.findViewById(R.id.report_post_description);
@@ -165,17 +180,15 @@ public class ReportPageActivity extends AppCompatActivity {
                 postTime = itemView.findViewById(R.id.report_time);
                 postImage = itemView.findViewById(R.id.report_post_image);
                 reportCommentButton = itemView.findViewById(R.id.report_comment_button);
+                DoneReportButton = itemView.findViewById(R.id.done_button);
 
-                // Set OnClickListener for "Report Comment" button
                 reportCommentButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        // Redirect to the ReportCommentsActivity and pass the PostKey as an extra
                         int position = getAdapterPosition();
                         if (position != RecyclerView.NO_POSITION) {
                             Reports reports = reportsList.get(position);
                             String postKey = reports.getPostKey();
-
                             Intent reportCommentsIntent = new Intent(v.getContext(), ReportCommentsActivity.class);
                             reportCommentsIntent.putExtra("PostKey", postKey);
                             v.getContext().startActivity(reportCommentsIntent);
@@ -185,22 +198,18 @@ public class ReportPageActivity extends AppCompatActivity {
             }
 
             public void setProfileImage(String imageUrl) {
-                // Use Picasso or any other image loading library to load the profile image into the CircleImageView
                 if (!TextUtils.isEmpty(imageUrl)) {
                     Picasso.get().load(imageUrl).into(profileImage, new Callback() {
                         @Override
                         public void onSuccess() {
-                            // Image loaded successfully
                         }
 
                         @Override
                         public void onError(Exception e) {
-                            // Handle the error if the image fails to load
                             e.printStackTrace();
                         }
                     });
                 } else {
-                    // Set a default placeholder image if the profile image URL is empty or null
                     profileImage.setImageResource(R.drawable.profile);
                 }
             }
@@ -214,22 +223,18 @@ public class ReportPageActivity extends AppCompatActivity {
             }
 
             public void setPostImage(String imageUrl) {
-                // Use Picasso or any other image loading library to load the post image into the ImageView
                 if (!TextUtils.isEmpty(imageUrl)) {
                     Picasso.get().load(imageUrl).into(postImage, new Callback() {
                         @Override
                         public void onSuccess() {
-                            // Image loaded successfully
                         }
 
                         @Override
                         public void onError(Exception e) {
-                            // Handle the error if the image fails to load
                             e.printStackTrace();
                         }
                     });
                 } else {
-                    // Set a default placeholder image if the post image URL is empty or null
                     postImage.setImageResource(R.drawable.select_image);
                 }
             }
@@ -240,6 +245,26 @@ public class ReportPageActivity extends AppCompatActivity {
 
             public void setPostTime(String time) {
                 postTime.setText(time);
+            }
+
+            public void setDoneButtonStatus(String postKey) {
+                DoneRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(postKey).hasChild(currentUserID)) {
+                            // User has Done (Liked) the report, set the DoneReportButton to "Liked" state
+                            DoneReportButton.setImageResource(R.drawable.baseline_check_box_24);
+                        } else {
+                            // User has not Done (Liked) the report, set the DoneReportButton to "Unliked" state
+                            DoneReportButton.setImageResource(R.drawable.baseline_check_box_outline_blank_24);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle onCancelled event
+                    }
+                });
             }
         }
     }
